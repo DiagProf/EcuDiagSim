77c569b5c8caa1adaa08dfaaa6809e5592f07923
DiagProf <vmax28575@gmail.com>
2023-01-30 18:24:27 +0100

    add

---
+++ EcuDiagSim/ApiLibLogging.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+
+
+namespace EcuDiagSim
+{
+    internal static class ApiLibLogging
+    {
+        //https://docs.microsoft.com/de-de/archive/msdn-magazine/2016/april/essential-net-logging-with-net-core
+        internal static ILoggerFactory LoggerFactory { get; set; } = NullLoggerFactory.Instance;
+
+        internal static ILogger CreateLogger<T>()
+        {
+            return LoggerFactory.CreateLogger<T>();
+        }
+    }
+}
